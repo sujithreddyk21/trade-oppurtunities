fe550e9f381cdf558ba9302879a09d7fd03d760a
sujithreddyk21 <sujithreddyk21@gmail.com>
2025-12-23 11:27:23 +0530

    security.py

--- security.py
+++ security.py
@@ -5,20 +5,20 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
 
-# CONFIG
+
 SECRET_KEY = "mysecretkey"  # Change this in production
 ALGORITHM = "HS256"
 
-# In-Memory Storage (Simulating a DB)
+
 users_db = {
-    "testuser": "$2b$12$GAjuPB8xCBChsyd0KoCbcegIe291FE1.6w/5BNRU6vAE33bBJr./."  # password: 'password123'
+    "testuser": ""  # password: 'password123'
 }
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBearer()
 
 def verify_token(credentials: HTTPAuthorizationCredentials = Security(security)):
-    """Decodes JWT and validates user."""
+    
     token = credentials.credentials
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -30,8 +30,9 @@ def verify_token(credentials: HTTPAuthorizationCredentials = Security(security))
         raise HTTPException(status_code=401, detail="Could not validate credentials")
 
 def create_access_token(data: dict):
-    """Creates a JWT token."""
+    
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
+
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
